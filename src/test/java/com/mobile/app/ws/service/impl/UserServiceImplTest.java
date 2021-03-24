8e84e01bc1bc7ea0aefc4083fd69790af035dd04
Jagannath Balachandran <jbalacha@In-JBalacha.local>
2021-03-24 16:04:53 +0530

    Enhanced create user test to check address details

--- src/test/java/com/mobile/app/ws/service/impl/UserServiceImplTest.java
+++ src/test/java/com/mobile/app/ws/service/impl/UserServiceImplTest.java
@@ -1,8 +1,8 @@
 package com.mobile.app.ws.service.impl;
 
+import com.mobile.app.ws.io.entity.AddressEntity;
 import com.mobile.app.ws.io.entity.UserEntity;
 import com.mobile.app.ws.repository.UserRepository;
-import com.mobile.app.ws.service.UserService;
 import com.mobile.app.ws.shared.dto.AddressDto;
 import com.mobile.app.ws.shared.dto.UserDto;
 import com.mobile.app.ws.utils.Util;
@@ -12,13 +12,13 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 public class UserServiceImplTest {
@@ -67,18 +67,18 @@ public class UserServiceImplTest {
     @Test
     void testCreateUser() {
         UserEntity userEntity = getUserEntity();
+        UserDto userTobeSaved = getUserDto();
 
         when(userRepository.findByEmail("jagannathbalachandran@gmail.com")).thenReturn(null);
         when(bCryptPasswordEncoder.encode(anyString())).thenReturn("password123");
         when(userRepository.save(any(UserEntity.class))).thenReturn(userEntity);
 
-
-        UserDto userTobeSaved = getUserDto();
-
         UserDto createdUser = userService.createUser(userTobeSaved);
 
+        Assertions.assertNotNull(createdUser);
         Assertions.assertEquals(userTobeSaved.getPassword() , createdUser.getPassword());
         Assertions.assertEquals(userTobeSaved.getEmail() , createdUser.getEmail());
+        Assertions.assertEquals(userTobeSaved.getAddresses().size() , createdUser.getAddresses().size());
 
     }
 
@@ -87,6 +87,7 @@ public class UserServiceImplTest {
         userTobeSaved.setEmail("jagannathbalachandran@gmail.com");
         userTobeSaved.setUserId("user123");
         userTobeSaved.setPassword("password123");
+
         List<AddressDto> addressDtoList = new ArrayList<>();
         AddressDto address1 = new AddressDto();
         address1.setAddressId("address123");
@@ -107,6 +108,19 @@ public class UserServiceImplTest {
         userEntity.setUserId("user123");
         userEntity.setEncryptedPassword("password123");
         userEntity.setEmail("jagannathbalachandran@gmail.com");
+        userEntity.setAddresses(getListOfAddressEntities());
         return userEntity;
+    }
+
+    private List<AddressEntity> getListOfAddressEntities() {
+        List<AddressEntity> addressEntities = new ArrayList<>();
+        AddressEntity addressEntity1 = new AddressEntity();
+        addressEntity1.setAddressId("address123");
+        addressEntity1.setCity("NY");
+        addressEntity1.setCountry("USA");
+        addressEntity1.setPostcode("12345");
+        addressEntity1.setState("NY");
+        addressEntities.add(addressEntity1);
+        return addressEntities;
     }
 }
